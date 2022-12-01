5972e7ea1128c8f5111f7f85a91fe855f11de98f
Ram047 <119288544+Ram047@users.noreply.github.com>
2022-12-01 22:14:05 +0530

    Create class6.py

---
+++ class6.py
@@ -0,0 +1,33 @@
+a=True
+if a:
+    print("This is true")
+if b:
+    print("this is false")
+else:
+    print("this is true")
+'''x:(-inf,inf)
+a= x <0
+b=x==0
+c=x>0'''
+# if conditions are mutually elcusive the use if,elif,else
+a="hrllo"
+b="world"
+c="hi"
+#KMAP
+
+#stored in truth table
+#question and answer
+'''
+a b c d q
+ 
+0 0 0 1 1
+1 0 0 0 1
+0 1 0 0 1
+0 1 0 1 1
+'''
+'''
+if (somestatement):
+    print("Has access")
+else:
+    print("Dont have access"))
+'''
